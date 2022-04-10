d31bf21252a2cfeb12cadc2e08cea3e63c2fe74d
rafaeladamou99 <rafaeladamou99@gmail.com>
2022-04-11 02:34:38 +0300

    Calculator App

--- CalculatorTutorial/ContentView.swift
+++ CalculatorTutorial/ContentView.swift
@@ -7,11 +7,197 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "x"
+    case equal = "="
+    case clear = "AC"
+    case decimal = "."
+    case percent = "%"
+    case negative = "-/+"
+    
+    var buttonColor: Color {
+        switch self {
+        case .add, .subtract, .multiply, .divide, .equal:
+            return .orange
+        case .clear, .negative, .percent:
+            return Color(.lightGray)
+        default:
+            return Color(UIColor(red: 55/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1))
+        }
     }
+}
+
+enum Operation {
+    case add, subtract, multiply, divide, none
+}
+
+struct ContentView: View {
+    
+    @State var value = "0"
+    @State var runningNumber = 0
+    @State var currentOperation: Operation = .none
+    
+    let buttons : [[CalcButton]] = [
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal],
+    ]
+    
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                //Text display
+                HStack {
+                    Spacer()
+                    Text(value)
+                        .bold()
+                        .font(.system(size:100))
+                        .foregroundColor(.white)
+                }
+                .padding()
+                
+                //Our buttons
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12) {
+                        ForEach(row, id: \.self) { item in
+                            Button(action: {
+                                self.didTap(button: item)
+                            }, label: {
+                                Text(item.rawValue)
+                                    .font(.system(size: 32))
+                                    .frame(
+                                        width: self.buttonWidth(item: item),
+                                        height: self.buttonHeigh()
+                                    )
+                                    .background(item.buttonColor)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(self.buttonWidth(item: item)/2)
+                            })
+                        }
+                    }
+                    .padding(.bottom, 3)
+                }
+            }
+        }
+    }
+    
+    func didTap(button: CalcButton){
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equal:
+            if button == .add {
+                self.currentOperation = .add
+                self.runningNumber = Int(self.value) ?? 0
+                
+            }
+            else if button == .subtract {
+                self.currentOperation = .subtract
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .equal {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                switch self.currentOperation {
+                case .add:
+                    self.value = "\(runningValue + currentValue)"
+                    self.currentOperation = .none
+                case .subtract:
+                    self.value = "\(runningValue - currentValue)"
+                    self.currentOperation = .none
+                case .multiply:
+                    self.value = "\(runningValue * currentValue)"
+                    self.currentOperation = .none
+                case .divide:
+                    if (currentValue == 0) {
+                        self.value = "undefined"
+                    }
+                    else {
+                        self.value = "\(runningValue / currentValue)"
+                    }
+                    self.currentOperation = .none
+                case .none:
+                    if currentValue == 110920 {
+                        self.value = "First Kiss"
+                    }
+                    else if currentValue == 020321{
+                        self.value = "First Blowjob"
+                    }
+                    else if currentValue == 090421{
+                        self.value = "Left Athens"
+                    }
+                    else if currentValue == 060321{
+                        self.value = "First Time"
+                    }
+                    else if currentValue == 190521{
+                        self.value = "Came to Athens"
+                    }
+                    else if currentValue == 250621{
+                        self.value = "Left Athens"
+                    }
+                    else if currentValue == 230821 {
+                        self.value = "Moved Together"
+                    }
+                    break
+                }
+            }
+            if button != .equal {
+                self.value = "0"
+            }
+        case .clear:
+            self.value = "0"
+        case .decimal, .negative, .percent:
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" || self.value == "undefined" || self.value == "First Kiss"
+            || self.value == "First Blowjob" || self.value == "Left Athens" ||
+                self.value == "First Time" || self.value == "Came to Athens" ||
+                self.value == "Moved Together"{
+                value = number
+            }
+            else {
+                self.value = "\(self.value)\(number)"
+            }
+        }
+    }
+    
+    func buttonWidth(item: CalcButton) -> CGFloat {
+        if item == .zero {
+            return ((UIScreen.main.bounds.width - (4*12)) / 4) * 2
+        }
+        return (UIScreen.main.bounds.width - (5*12)) / 4
+    }
+    
+    func buttonHeigh() -> CGFloat {
+        return (UIScreen.main.bounds.width - (5*12)) / 4
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
