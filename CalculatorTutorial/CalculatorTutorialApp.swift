44e8071b7e25f6540040b9c5d35339d5dff075ec
rafaeladamou99 <rafaeladamou99@gmail.com>
2022-04-10 18:02:51 +0300

    Initial Commit

---
+++ CalculatorTutorial/CalculatorTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorTutorialApp.swift
+//  CalculatorTutorial
+//
+//  Created by Rafael Adamou on 10/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
